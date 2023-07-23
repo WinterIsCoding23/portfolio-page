5aaca3bf27681568ce11208f06817e6bcd66edef
Markus Winter <markusdominicwinter@hotmail.com>
2023-07-23 18:33:21 +0200

    create Navbar & import it into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 
+import Navbar from "./Components/Navbar";
+
 function App() {
-  return <BrowserRouter>Hello World!</BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return (
+    <div>
+      <h1>This is my Navbar</h1>
+      <p>Hello World</p>
+    </div>
+  );
+}
