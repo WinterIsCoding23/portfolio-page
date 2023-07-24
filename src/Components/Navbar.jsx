920fd8ef168b9c39c48b2a1a6b34bcc3ed200442
Markus Winter <markusdominicwinter@hotmail.com>
2023-07-24 15:58:16 +0200

    add padding to logo-placeholder & links

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,23 +7,28 @@ export default function Navbar() {
   const [toggle, setToggle] = useState(false);
 
   return (
-    <nav className="w-full flex items-center py-5 fixed top-0 z-201q">
-      <ul className="list-none hidden sm:flex flex-row gap-10">
-        {navLinks.map((link) => (
-          <li
-            key={link.id}
-            className={`${
-              active === link.title ? "text-white" : "text-sky-500"
-            } font-poppins font-medium cursor-pointer text-[16px]`}
-            onClick={() => {
-              setToggle(!toggle);
-              setActive(link.title);
-            }}
-          >
-            <a href={`#${link.id}`}>{link.title}</a>
-          </li>
-        ))}
-      </ul>
+    <nav className="w-full flex items-center py-5 fixed top-0 z-20 pl-1 pr-1">
+      <div className="w-full flex justify-between ">
+        <div className="pl-1">Logo</div>
+        <div className="pr-1">
+          <ul className="list-none hidden sm:flex flex-row gap-10">
+            {navLinks.map((link) => (
+              <li
+                key={link.id}
+                className={`${
+                  active === link.title ? "text-white" : "text-sky-500"
+                } font-poppins font-medium cursor-pointer text-[16px]`}
+                onClick={() => {
+                  setToggle(!toggle);
+                  setActive(link.title);
+                }}
+              >
+                <a href={`#${link.id}`}>{link.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 }
