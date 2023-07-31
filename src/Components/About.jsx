b343cfd2b8fa676b40b0fcfff5e4ebca52c50b06
Markus Winter <markusdominicwinter@hotmail.com>
2023-07-31 18:39:49 +0200

    make about-me-paragraph responsive

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,7 +10,7 @@ export default function About() {
         <h3 className={styles.sectionHeadText}>About me</h3>
       </motion.div>
 
-      <motion.p className="text-[18px] tracking-normal text-left mt-2 text-paragraph">
+      <motion.p className="text-[15px] sm:text-[18px] lg:text-[25px] tracking-normal text-left mt-2 text-paragraph">
         Self-description Lorem ipsum dolor sit amet, consetetur sadipscing
         elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
         aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
