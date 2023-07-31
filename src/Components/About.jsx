1e7f31085c439739d609fbbe8c9f5b686bae374e
Markus Winter <markusdominicwinter@hotmail.com>
2023-07-31 18:49:55 +0200

    define fadeIn-animation from framer-motion & apply it to about-me-paragraph

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,6 +2,7 @@ import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 
 import styles from "../styles";
+import { fadeIn } from "../utils/motion";
 
 export default function About() {
   return (
@@ -10,7 +11,10 @@ export default function About() {
         <h3 className={styles.sectionHeadText}>About me</h3>
       </motion.div>
 
-      <motion.p className="text-[15px] sm:text-[18px] lg:text-[25px] tracking-normal text-left mt-2 text-paragraph">
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="text-[15px] sm:text-[18px] lg:text-[25px] tracking-normal text-left mt-2 text-paragraph"
+      >
         Self-description Lorem ipsum dolor sit amet, consetetur sadipscing
         elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
         aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
